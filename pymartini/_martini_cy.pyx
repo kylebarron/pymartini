434e33d981a03eb28d34e9db40918eb19f19dc00
Kyle Barron <kylebarron2@gmail.com>
2020-05-26 23:10:28 -0600

    Add simple types for crazy speedups!!

--- pymartini/_martini_cy.pyx
+++ pymartini/_martini_cy.pyx
@@ -16,6 +16,12 @@ def martini(int grid_size):
     # coordinates for all possible triangles in an RTIN tile
     coords = np.zeros(num_triangles * 4, dtype=np.uint16)
 
+    # Py_ssize_t is the proper C type for Python array indices.
+    cdef Py_ssize_t i, _id
+    cdef unsigned short [:] coords_view = coords
+    cdef int k
+    cdef unsigned short ax, ay, bx, by, mx, my, cx, cy
+
     # get triangle coordinates from its index in an implicit binary tree
     for i in range(num_triangles):
         # id is a reserved name in Python
@@ -48,22 +54,38 @@ def martini(int grid_size):
             cx, cy = mx, my
 
         k = i * 4
-        coords[k + 0] = ax
-        coords[k + 1] = ay
-        coords[k + 2] = bx
-        coords[k + 3] = by
+        coords_view[k + 0] = ax
+        coords_view[k + 1] = ay
+        coords_view[k + 2] = bx
+        coords_view[k + 3] = by
 
     return grid_size, num_triangles, num_parent_triangles, indices, coords
 
 
-def tile_update(num_triangles, num_parent_triangles, coords, size, terrain, errors):
+def tile_update(
+    int num_triangles,
+    int num_parent_triangles,
+    np.ndarray[np.uint16_t, ndim=1] coords,
+    int size,
+    np.ndarray[np.float32_t, ndim=1] terrain,
+    np.ndarray[np.float32_t, ndim=1] errors):
+
+    cdef unsigned short [:] coords_view = coords
+    cdef float [:] terrain_view = terrain
+    cdef float [:] errors_view = errors
+
+    # Py_ssize_t is the proper C type for Python array indices.
+    cdef Py_ssize_t i
+    cdef int k
+    cdef unsigned short ax, ay, bx, by, mx, my, cx, cy
+
     # iterate over all possible triangles, starting from the smallest level
     for i in range(num_triangles - 1, -1, -1):
         k = i * 4
-        ax = coords[k + 0]
-        ay = coords[k + 1]
-        bx = coords[k + 2]
-        by = coords[k + 3]
+        ax = coords_view[k + 0]
+        ay = coords_view[k + 1]
+        bx = coords_view[k + 2]
+        by = coords_view[k + 3]
         mx = (ax + bx) >> 1
         my = (ay + by) >> 1
         cx = mx + my - ay
@@ -71,18 +93,18 @@ def tile_update(num_triangles, num_parent_triangles, coords, size, terrain, erro
 
         # calculate error in the middle of the long edge of the triangle
         interpolated_height = (
-            terrain[ay * size + ax] + terrain[by * size + bx]) / 2
+            terrain_view[ay * size + ax] + terrain_view[by * size + bx]) / 2
         middle_index = my * size + mx
-        middle_error = abs(interpolated_height - terrain[middle_index])
+        middle_error = abs(interpolated_height - terrain_view[middle_index])
 
-        errors[middle_index] = max(errors[middle_index], middle_error)
+        errors_view[middle_index] = max(errors_view[middle_index], middle_error)
 
         if i < num_parent_triangles:
             # bigger triangles; accumulate error with children
             left_child_index = ((ay + cy) >> 1) * size + ((ax + cx) >> 1)
             right_child_index = ((by + cy) >> 1) * size + ((bx + cx) >> 1)
-            errors[middle_index] = max(
-                errors[middle_index], errors[left_child_index],
-                errors[right_child_index])
+            errors_view[middle_index] = max(
+                errors_view[middle_index], errors_view[left_child_index],
+                errors_view[right_child_index])
 
     return errors
