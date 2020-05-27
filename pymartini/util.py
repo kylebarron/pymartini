eede4e94ab11eff46d1229a8128e3cc4ed833989
Kyle Barron <kylebarron2@gmail.com>
2020-05-26 20:50:15 -0600

    Faster decoding terrain values with broadcasting

--- pymartini/util.py
+++ pymartini/util.py
@@ -2,26 +2,22 @@ import numpy as np
 
 
 def mapbox_terrain_to_grid(png):
-    tile_size = png.shape[0]
-    grid_size = tile_size + 1
-    terrain = np.zeros(grid_size * grid_size, dtype=np.float32)
+    grid_size = png.shape[0] + 1
+    terrain = np.zeros((grid_size, grid_size), dtype=np.float32)
 
-    # decode terrain values
-    for y in range(tile_size):
-        for x in range(tile_size):
-            r = png[y, x, 0]
-            g = png[y, x, 1]
-            b = png[y, x, 2]
-            terrain[y * tile_size +
-                    x] = (r * 256 * 256 + g * 256.0 + b) / 10.0 - 10000.0
+    # Get bands
+    red = png[:, :, 0] * (256 * 256)
+    green = png[:, :, 1] * (256)
+    blue = png[:, :, 2]
+
+    # Compute float height
+    heights = (red + green + blue) / 10 - 10000
+
+    # Copy to larger array to allow backfilling
+    np.copyto(terrain[:512, :512], heights)
 
     # backfill right and bottom borders
-    for x in range(grid_size - 1):
-        terrain[grid_size * (grid_size - 1) + x] = terrain[grid_size *
-                                                           (grid_size - 2) + x]
+    terrain[grid_size - 1, :] = terrain[grid_size - 2, :]
+    terrain[:, grid_size - 1] = terrain[:, grid_size - 2]
 
-    for y in range(grid_size):
-        terrain[grid_size * y + grid_size - 1] = terrain[grid_size * y +
-                                                         grid_size - 2]
-
-    return terrain
+    return terrain.flatten('C')
