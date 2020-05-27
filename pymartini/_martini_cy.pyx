4a70e1fb8eb612831da4558fe45166179d772d9b
Kyle Barron <kylebarron2@gmail.com>
2020-05-27 00:06:25 -0600

    Port get_mesh to cython

--- pymartini/_martini_cy.pyx
+++ pymartini/_martini_cy.pyx
@@ -11,10 +11,10 @@ def martini(int grid_size):
     num_triangles = tile_size * tile_size * 2 - 2
     num_parent_triangles = num_triangles - tile_size * tile_size
 
-    indices = np.zeros(grid_size * grid_size, dtype=np.uint32)
+    cdef np.ndarray[np.uint32_t, ndim=1] indices = np.zeros(grid_size * grid_size, dtype=np.uint32)
 
     # coordinates for all possible triangles in an RTIN tile
-    coords = np.zeros(num_triangles * 4, dtype=np.uint16)
+    cdef np.ndarray[np.uint16_t, ndim=1] coords = np.zeros(num_triangles * 4, dtype=np.uint16)
 
     # Py_ssize_t is the proper C type for Python array indices.
     cdef Py_ssize_t i, _id
@@ -110,6 +110,41 @@ def tile_update(
     return errors
 
 
+def get_mesh(
+      np.ndarray[np.float32_t, ndim=1] errors,
+      np.ndarray[np.uint32_t, ndim=1] indices,
+      unsigned short size,
+      float max_error,
+    ):
+
+    cdef unsigned int num_vertices = 0
+    cdef unsigned int num_triangles = 0
+    # max is a reserved keyword in Python
+    cdef unsigned short _max = size - 1
+
+    # use an index grid to keep track of vertices that were already used to
+    # avoid duplication
+    # I already initialized array with zeros
+    # indices.fill(0)
+
+    # retrieve mesh in two stages that both traverse the error map:
+    # - countElements: find used vertices (and assign each an index), and count triangles (for minimum allocation)
+    # - processTriangle: fill the allocated vertices & triangles typed arrays
+
+    num_vertices, num_triangles, errors, indices = countElements(
+        0, 0, _max, _max, _max, 0, num_vertices, num_triangles, errors, indices, max_error, size)
+    num_vertices, num_triangles, errors, indices = countElements(
+        _max, _max, 0, 0, 0, _max, num_vertices, num_triangles, errors, indices, max_error, size)
+
+    cdef np.ndarray[np.uint16_t, ndim=1] vertices = np.zeros(num_vertices * 2, dtype=np.uint16)
+    cdef np.ndarray[np.uint32_t, ndim=1] triangles = np.zeros(num_triangles * 3, dtype=np.uint32)
+    cdef unsigned int tri_index = 0
+
+    triangles, vertices, tri_index = processTriangle(0, 0, _max, _max, _max, 0, tri_index, errors, indices, triangles, vertices, max_error, size)
+    triangles, vertices, tri_index = processTriangle(_max, _max, 0, 0, 0, _max, tri_index, errors, indices, triangles, vertices, max_error, size)
+
+    return vertices, triangles
+
 def countElements(
     unsigned short ax,
     unsigned short ay,
@@ -151,3 +186,59 @@ def countElements(
         num_triangles += 1
 
     return num_vertices, num_triangles, errors, indices
+
+
+def processTriangle(
+    unsigned short ax,
+    unsigned short ay,
+    unsigned short bx,
+    unsigned short by,
+    unsigned short cx,
+    unsigned short cy,
+    unsigned int tri_index,
+    np.ndarray[np.float32_t, ndim=1] errors,
+    np.ndarray[np.uint32_t, ndim=1] indices,
+    np.ndarray[np.uint32_t, ndim=1] triangles,
+    np.ndarray[np.uint16_t, ndim=1] vertices,
+    float max_error,
+    unsigned int size
+    ):
+
+    cdef unsigned short mx, my
+    cdef float [:] errors_view = errors
+    cdef unsigned int [:] indices_view = indices
+    cdef unsigned int [:] triangles_view = triangles
+    cdef unsigned short [:] vertices_view = vertices
+
+    mx = (ax + bx) >> 1
+    my = (ay + by) >> 1
+
+    if (abs(ax - cx) + abs(ay - cy) > 1) and (errors_view[my * size + mx] >
+                                              max_error):
+        # triangle doesn't approximate the surface well enough; drill down further
+        triangles, vertices, tri_index = processTriangle(cx, cy, ax, ay, mx, my, tri_index, errors, indices, triangles, vertices, max_error, size)
+        triangles, vertices, tri_index = processTriangle(bx, by, cx, cy, mx, my, tri_index, errors, indices, triangles, vertices, max_error, size)
+
+    else:
+        # add a triangle
+        a = indices_view[ay * size + ax] - 1
+        b = indices_view[by * size + bx] - 1
+        c = indices_view[cy * size + cx] - 1
+
+        vertices_view[2 * a] = ax
+        vertices_view[2 * a + 1] = ay
+
+        vertices_view[2 * b] = bx
+        vertices_view[2 * b + 1] = by
+
+        vertices_view[2 * c] = cx
+        vertices_view[2 * c + 1] = cy
+
+        triangles_view[tri_index] = a
+        tri_index += 1
+        triangles_view[tri_index] = b
+        tri_index += 1
+        triangles_view[tri_index] = c
+        tri_index += 1
+
+    return triangles, vertices, tri_index
--- pymartini/martini.py
+++ pymartini/martini.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from ._martini_cy import martini as _martini
-from ._martini_cy import tile_update, countElements
+from ._martini_cy import get_mesh as _get_mesh
+from ._martini_cy import tile_update, countElements, processTriangle
 
 
 class Martini:
@@ -37,68 +38,9 @@ class Tile:
             errors=self.errors)
 
     def get_mesh(self, max_error=0):
-        size = self.martini.grid_size
-        indices = self.martini.indices
-        errors = self.errors
-
-        num_vertices = 0
-        num_triangles = 0
-        # max is a reserved keyword in Python
-        _max = size - 1
-
-        # use an index grid to keep track of vertices that were already used to
-        # avoid duplication
-        # I already initialized array with zeros
-        # indices.fill(0)
-
-        # retrieve mesh in two stages that both traverse the error map:
-        # - countElements: find used vertices (and assign each an index), and count triangles (for minimum allocation)
-        # - processTriangle: fill the allocated vertices & triangles typed arrays
-
-        num_vertices, num_triangles, errors, indices = countElements(
-            0, 0, _max, _max, _max, 0, num_vertices, num_triangles, errors, indices, max_error, size)
-        num_vertices, num_triangles, errors, indices = countElements(
-            _max, _max, 0, 0, 0, _max, num_vertices, num_triangles, errors, indices, max_error, size)
-
-        vertices = np.zeros(num_vertices * 2, dtype=np.uint16)
-        triangles = np.zeros(num_triangles * 3, dtype=np.uint32)
-        tri_index = 0
-
-        def processTriangle(ax, ay, bx, by, cx, cy, tri_index):
-            mx = (ax + bx) >> 1
-            my = (ay + by) >> 1
-
-            if (abs(ax - cx) + abs(ay - cy) > 1) and (errors[my * size + mx] >
-                                                      max_error):
-                # triangle doesn't approximate the surface well enough; drill down further
-                tri_index = processTriangle(cx, cy, ax, ay, mx, my, tri_index)
-                tri_index = processTriangle(bx, by, cx, cy, mx, my, tri_index)
-
-            else:
-                # add a triangle
-                a = indices[ay * size + ax] - 1
-                b = indices[by * size + bx] - 1
-                c = indices[cy * size + cx] - 1
-
-                vertices[2 * a] = ax
-                vertices[2 * a + 1] = ay
-
-                vertices[2 * b] = bx
-                vertices[2 * b + 1] = by
-
-                vertices[2 * c] = cx
-                vertices[2 * c + 1] = cy
-
-                triangles[tri_index] = a
-                tri_index += 1
-                triangles[tri_index] = b
-                tri_index += 1
-                triangles[tri_index] = c
-                tri_index += 1
-
-            return tri_index
-
-        tri_index = processTriangle(0, 0, _max, _max, _max, 0, tri_index)
-        tri_index = processTriangle(_max, _max, 0, 0, 0, _max, tri_index)
-
-        return vertices, triangles
+        return _get_mesh(
+          errors=self.errors,
+          indices=self.martini.indices,
+          size=self.martini.grid_size,
+          max_error=max_error
+        )
