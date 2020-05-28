8eedb631a4e3997a469373eedd06a6fba6a6db1b
Kyle Barron <kylebarron2@gmail.com>
2020-05-27 22:42:15 -0600

    Port countElements and processTriangles to class methods Made `tile.get_mesh(5)` go from 3.2s to 13ms!

--- pymartini/martini.pyx
+++ pymartini/martini.pyx
@@ -5,7 +5,7 @@ cimport numpy as np
 cdef class Martini:
     # Define class attributes
     cdef readonly unsigned short grid_size
-    cdef readonly unsigned int num_triangles
+    cdef readonly unsigned int max_num_triangles
     cdef readonly unsigned int num_parent_triangles
 
     # Can't store Numpy arrays as class attributes, but you _can_ store the
@@ -21,13 +21,13 @@ cdef class Martini:
             raise ValueError(
                 f'Expected grid size to be 2^n+1, got {grid_size}.')
 
-        self.num_triangles = tile_size * tile_size * 2 - 2
-        self.num_parent_triangles = self.num_triangles - tile_size * tile_size
+        self.max_num_triangles = tile_size * tile_size * 2 - 2
+        self.num_parent_triangles = self.max_num_triangles - tile_size * tile_size
 
         cdef np.ndarray[np.uint32_t, ndim=1] indices = np.zeros(grid_size * grid_size, dtype=np.uint32)
 
         # coordinates for all possible triangles in an RTIN tile
-        cdef np.ndarray[np.uint16_t, ndim=1] coords = np.zeros(self.num_triangles * 4, dtype=np.uint16)
+        cdef np.ndarray[np.uint16_t, ndim=1] coords = np.zeros(self.max_num_triangles * 4, dtype=np.uint16)
 
         # Py_ssize_t is the proper C type for Python array indices.
         cdef Py_ssize_t i, _id
@@ -38,7 +38,7 @@ cdef class Martini:
         cdef unsigned short ax, ay, bx, by, mx, my, cx, cy
 
         # get triangle coordinates from its index in an implicit binary tree
-        for i in range(self.num_triangles):
+        for i in range(self.max_num_triangles):
             # id is a reserved name in Python
             _id = i + 2
 
@@ -84,7 +84,7 @@ cdef class Martini:
 cdef class Tile:
     # Define class attributes
     cdef readonly unsigned short grid_size
-    cdef readonly unsigned int num_triangles
+    cdef readonly unsigned int max_num_triangles
     cdef readonly unsigned int num_parent_triangles
 
     # Can't store Numpy arrays as class attributes, but you _can_ store the
@@ -95,6 +95,15 @@ cdef class Tile:
 
     cdef public np.float32_t[:] terrain_view
     cdef public np.float32_t[:] errors_view
+
+    # "globals" Used in getMesh
+    # TODO try not public
+    cdef public unsigned int num_vertices
+    cdef public unsigned int num_triangles
+    cdef public float max_error
+    cdef unsigned int tri_index
+    cdef public np.uint16_t[:] vertices_view
+    cdef np.uint32_t[:] triangles_view
 
     def __init__(self, terrain, martini):
         size = martini.grid_size
@@ -109,7 +118,7 @@ cdef class Tile:
 
         # Expand Martini instance, since I can't cdef a class
         self.grid_size = martini.grid_size
-        self.num_triangles = martini.num_triangles
+        self.max_num_triangles = martini.max_num_triangles
         self.num_parent_triangles = martini.num_parent_triangles
         self.indices_view = martini.indices_view
         self.coords_view = martini.coords_view
@@ -126,7 +135,7 @@ cdef class Tile:
         cdef unsigned short ax, ay, bx, by, mx, my, cx, cy
 
         # iterate over all possible triangles, starting from the smallest level
-        for i in range(self.num_triangles - 1, -1, -1):
+        for i in range(self.max_num_triangles - 1, -1, -1):
             k = i * 4
             ax = self.coords_view[k + 0]
             ay = self.coords_view[k + 1]
@@ -153,147 +162,117 @@ cdef class Tile:
                     self.errors_view[middle_index], self.errors_view[left_child_index],
                     self.errors_view[right_child_index])
 
-    def get_mesh(self, max_error=0):
-        indices = np.asarray(self.indices_view, dtype=np.uint32)
-        errors = np.asarray(self.errors_view, dtype=np.float32)
+    def get_mesh(self, float max_error=0):
+        cdef unsigned short size
+        size = self.grid_size
 
-        return get_mesh(
-          errors=errors,
-          indices=indices,
-          size=self.grid_size,
-          max_error=max_error
-        )
+        # Initialize to zero on each iteration
+        self.num_vertices = 0
+        self.num_triangles = 0
+        self.max_error = max_error
 
+        # max is a reserved keyword in Python
+        cdef unsigned short _max = size - 1
 
-def get_mesh(
-      np.ndarray[np.float32_t, ndim=1] errors,
-      np.ndarray[np.uint32_t, ndim=1] indices,
-      unsigned short size,
-      float max_error,
-    ):
+        # use an index grid to keep track of vertices that were already used to
+        # avoid duplication
+        # I already initialized array with zeros
+        # indices.fill(0)
 
-    cdef unsigned int num_vertices = 0
-    cdef unsigned int num_triangles = 0
-    # max is a reserved keyword in Python
-    cdef unsigned short _max = size - 1
+        # retrieve mesh in two stages that both traverse the error map:
+        # - countElements: find used vertices (and assign each an index), and count triangles (for minimum allocation)
+        # - processTriangle: fill the allocated vertices & triangles typed arrays
 
-    # use an index grid to keep track of vertices that were already used to
-    # avoid duplication
-    # I already initialized array with zeros
-    # indices.fill(0)
+        self.countElements(0, 0, _max, _max, _max, 0)
+        self.countElements(_max, _max, 0, 0, 0, _max)
 
-    # retrieve mesh in two stages that both traverse the error map:
-    # - countElements: find used vertices (and assign each an index), and count triangles (for minimum allocation)
-    # - processTriangle: fill the allocated vertices & triangles typed arrays
+        self.vertices_view = np.zeros(self.num_vertices * 2, dtype=np.uint16)
+        self.triangles_view = np.zeros(self.num_triangles * 3, dtype=np.uint32)
+        self.tri_index = 0
 
-    num_vertices, num_triangles, errors, indices = countElements(
-        0, 0, _max, _max, _max, 0, num_vertices, num_triangles, errors, indices, max_error, size)
-    num_vertices, num_triangles, errors, indices = countElements(
-        _max, _max, 0, 0, 0, _max, num_vertices, num_triangles, errors, indices, max_error, size)
+        self.processTriangle(0, 0, _max, _max, _max, 0)
+        self.processTriangle(_max, _max, 0, 0, 0, _max)
 
-    cdef np.ndarray[np.uint16_t, ndim=1] vertices = np.zeros(num_vertices * 2, dtype=np.uint16)
-    cdef np.ndarray[np.uint32_t, ndim=1] triangles = np.zeros(num_triangles * 3, dtype=np.uint32)
-    cdef unsigned int tri_index = 0
+        vertices = np.asarray(self.vertices_view, dtype=np.uint16)
+        triangles = np.asarray(self.triangles_view, dtype=np.uint32)
 
-    triangles, vertices, tri_index = processTriangle(0, 0, _max, _max, _max, 0, tri_index, errors, indices, triangles, vertices, max_error, size)
-    triangles, vertices, tri_index = processTriangle(_max, _max, 0, 0, 0, _max, tri_index, errors, indices, triangles, vertices, max_error, size)
+        return vertices, triangles
 
-    return vertices, triangles
+    cdef countElements(
+        self,
+        unsigned short ax,
+        unsigned short ay,
+        unsigned short bx,
+        unsigned short by,
+        unsigned short cx,
+        unsigned short cy):
 
-cdef countElements(
-    unsigned short ax,
-    unsigned short ay,
-    unsigned short bx,
-    unsigned short by,
-    unsigned short cx,
-    unsigned short cy,
-    unsigned int num_vertices,
-    unsigned int num_triangles,
-    np.ndarray[np.float32_t, ndim=1] errors,
-    np.ndarray[np.uint32_t, ndim=1] indices,
-    float max_error,
-    unsigned int size):
+        cdef unsigned short size
+        size = self.grid_size
 
-    cdef unsigned short mx, my
-    cdef float [:] errors_view = errors
-    cdef unsigned int [:] indices_view = indices
+        cdef unsigned short mx, my
 
-    mx = (ax + bx) >> 1
-    my = (ay + by) >> 1
+        mx = (ax + bx) >> 1
+        my = (ay + by) >> 1
 
-    if (abs(ax - cx) + abs(ay - cy) > 1) and (errors_view[my * size + mx] >
-                                              max_error):
-        num_vertices, num_triangles, errors, indices = countElements(
-            cx, cy, ax, ay, mx, my, num_vertices, num_triangles, errors, indices, max_error, size)
-        num_vertices, num_triangles, errors, indices = countElements(
-            bx, by, cx, cy, mx, my, num_vertices, num_triangles, errors, indices, max_error, size)
-    else:
-        if not indices_view[ay * size + ax]:
-            num_vertices += 1
-            indices_view[ay * size + ax] = num_vertices
-        if not indices_view[by * size + bx]:
-            num_vertices += 1
-            indices_view[by * size + bx] = num_vertices
-        if not indices_view[cy * size + cx]:
-            num_vertices += 1
-            indices_view[cy * size + cx] = num_vertices
+        if (abs(ax - cx) + abs(ay - cy) > 1) and (self.errors_view[my * size + mx] > self.max_error):
+            self.countElements(cx, cy, ax, ay, mx, my)
+            self.countElements(bx, by, cx, cy, mx, my)
+        else:
+            if not self.indices_view[ay * size + ax]:
+                self.num_vertices += 1
+                self.indices_view[ay * size + ax] = self.num_vertices
+            if not self.indices_view[by * size + bx]:
+                self.num_vertices += 1
+                self.indices_view[by * size + bx] = self.num_vertices
+            if not self.indices_view[cy * size + cx]:
+                self.num_vertices += 1
+                self.indices_view[cy * size + cx] = self.num_vertices
 
-        num_triangles += 1
+            self.num_triangles += 1
 
-    return num_vertices, num_triangles, errors, indices
+    cdef processTriangle(
+        self,
+        unsigned short ax,
+        unsigned short ay,
+        unsigned short bx,
+        unsigned short by,
+        unsigned short cx,
+        unsigned short cy,
+        ):
 
+        cdef unsigned short size
+        size = self.grid_size
 
-cdef processTriangle(
-    unsigned short ax,
-    unsigned short ay,
-    unsigned short bx,
-    unsigned short by,
-    unsigned short cx,
-    unsigned short cy,
-    unsigned int tri_index,
-    np.ndarray[np.float32_t, ndim=1] errors,
-    np.ndarray[np.uint32_t, ndim=1] indices,
-    np.ndarray[np.uint32_t, ndim=1] triangles,
-    np.ndarray[np.uint16_t, ndim=1] vertices,
-    float max_error,
-    unsigned int size
-    ):
+        cdef unsigned short mx, my
+        cdef unsigned int a, b, c
 
-    cdef unsigned short mx, my
-    cdef float [:] errors_view = errors
-    cdef unsigned int [:] indices_view = indices
-    cdef unsigned int [:] triangles_view = triangles
-    cdef unsigned short [:] vertices_view = vertices
+        mx = (ax + bx) >> 1
+        my = (ay + by) >> 1
 
-    mx = (ax + bx) >> 1
-    my = (ay + by) >> 1
+        if (abs(ax - cx) + abs(ay - cy) > 1) and (self.errors_view[my * size + mx] > self.max_error):
+            # triangle doesn't approximate the surface well enough; drill down further
+            self.processTriangle(cx, cy, ax, ay, mx, my)
+            self.processTriangle(bx, by, cx, cy, mx, my)
 
-    if (abs(ax - cx) + abs(ay - cy) > 1) and (errors_view[my * size + mx] >
-                                              max_error):
-        # triangle doesn't approximate the surface well enough; drill down further
-        triangles, vertices, tri_index = processTriangle(cx, cy, ax, ay, mx, my, tri_index, errors, indices, triangles, vertices, max_error, size)
-        triangles, vertices, tri_index = processTriangle(bx, by, cx, cy, mx, my, tri_index, errors, indices, triangles, vertices, max_error, size)
+        else:
+            # add a triangle
+            a = self.indices_view[ay * size + ax] - 1
+            b = self.indices_view[by * size + bx] - 1
+            c = self.indices_view[cy * size + cx] - 1
 
-    else:
-        # add a triangle
-        a = indices_view[ay * size + ax] - 1
-        b = indices_view[by * size + bx] - 1
-        c = indices_view[cy * size + cx] - 1
+            self.vertices_view[2 * a] = ax
+            self.vertices_view[2 * a + 1] = ay
 
-        vertices_view[2 * a] = ax
-        vertices_view[2 * a + 1] = ay
+            self.vertices_view[2 * b] = bx
+            self.vertices_view[2 * b + 1] = by
 
-        vertices_view[2 * b] = bx
-        vertices_view[2 * b + 1] = by
+            self.vertices_view[2 * c] = cx
+            self.vertices_view[2 * c + 1] = cy
 
-        vertices_view[2 * c] = cx
-        vertices_view[2 * c + 1] = cy
-
-        triangles_view[tri_index] = a
-        tri_index += 1
-        triangles_view[tri_index] = b
-        tri_index += 1
-        triangles_view[tri_index] = c
-        tri_index += 1
-
-    return triangles, vertices, tri_index
+            self.triangles_view[self.tri_index] = a
+            self.tri_index += 1
+            self.triangles_view[self.tri_index] = b
+            self.tri_index += 1
+            self.triangles_view[self.tri_index] = c
+            self.tri_index += 1
