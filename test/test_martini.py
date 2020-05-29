53c3591f936bed5ad4dedbec816e0c7e64739f11
Kyle Barron <kylebarron2@gmail.com>
2020-05-28 18:36:39 -0600

    Add test case for errors array

--- test/test_martini.py
+++ test/test_martini.py
@@ -13,48 +13,45 @@ for png_fname, encoding in TEST_PNG_FILES:
     for max_error in [5, 20, 50, 100, 500]:
         TEST_CASES.append([png_fname, max_error, encoding])
 
+def this_dir():
+    try:
+        return Path(__file__).resolve().parents[0]
+    except NameError:
+        return Path('.').resolve()
+
+
 @pytest.mark.parametrize("png_fname,encoding", TEST_PNG_FILES)
 def test_terrain(png_fname, encoding):
     """Test output from decode_ele against JS output
     """
-    # Generate terrain output
-    path = Path(__file__).parents[0] / f'data/{png_fname}.png'
+    # Generate terrain output in Python
+    path = this_dir() / f'data/{png_fname}.png'
     png = imread(path)
     terrain = decode_ele(png, encoding=encoding)
 
     # Load JS terrain output
-    path = Path(__file__).parents[0] / f'data/{png_fname}_terrain'
+    path = this_dir() / f'data/{png_fname}_terrain'
     with open(path, 'rb') as f:
         exp_terrain = np.frombuffer(f.read(), dtype=np.float32)
 
     assert np.array_equal(terrain, exp_terrain), 'terrain not matching expected'
 
 
-@pytest.mark.parametrize("png_fname,max_error,encoding", TEST_CASES)
-def test_mesh(png_fname, max_error, encoding):
-    path = Path(__file__).parents[0] / f'data/{png_fname}.png'
-
+@pytest.mark.parametrize("png_fname,encoding", TEST_PNG_FILES)
+def test_errors(png_fname, encoding):
+    """Test errors output from martini.create_tile(terrain)
+    """
+    # Generate errors output in Python
+    path = this_dir() / f'data/{png_fname}.png'
     png = imread(path)
     terrain = decode_ele(png, encoding=encoding)
-
     martini = Martini(png.shape[0] + 1)
     tile = martini.create_tile(terrain)
-    vertices, triangles = tile.get_mesh(max_error)
+    errors = np.asarray(tile.errors_view, dtype=np.float32)
 
-    # Load JS data
-    with open(Path(__file__).parents[0] /
-              f'data/{png_fname}_{max_error}.json') as f:
-        data = json.load(f)
+    # Load JS errors output
+    path = this_dir() / f'data/{png_fname}_errors'
+    with open(path, 'rb') as f:
+        exp_errors = np.frombuffer(f.read(), dtype=np.float32)
 
-    msg = '`vertices` length does not match'
-    assert len(vertices) == len(data['vertices']), msg
-    assert np.array_equal(
-        vertices, np.asarray(data['vertices'],
-                             dtype=np.uint16)), '`vertices` values do not match'
-
-    msg = '`triangles` length does not match'
-    assert len(triangles) == len(data['triangles']), msg
-    assert np.array_equal(
-        triangles,
-        np.asarray(data['triangles'],
-                   dtype=np.uint16)), '`triangles` values do not match'
+    assert np.array_equal(errors, exp_errors), 'errors not matching expected'
