a45edf60631f92625184e6a585a00a1f06e76de6
Kyle Barron <kylebarron2@gmail.com>
2020-06-01 01:23:54 -0600

    Bump version: 0.2.0 → 0.2.1

--- pymartini/__init__.py
+++ pymartini/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Kyle Barron"""
 __email__ = 'kylebarron2@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .martini import Martini
 from .util import decode_ele
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def find_pyx(path='.'):
 
 setup(
     name="pymartini",
-    version="0.2.0",
+    version="0.2.1",
     description="A Python port of Martini for fast terrain mesh generation",
     long_description=readme,
     long_description_content_type="text/markdown",
