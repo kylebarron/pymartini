b96a240a3df7bda17355f3cae5e4ed7f8195c5ca
Kyle Barron <kylebarron2@gmail.com>
2020-06-01 23:38:35 -0600

    Make backfilling optional Closes #12

--- pymartini/util.py
+++ pymartini/util.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def decode_ele(png, encoding):
+def decode_ele(png, encoding, backfill=True):
     """Decode array to elevations
 
     Arguments:
@@ -10,6 +10,9 @@ def decode_ele(png, encoding):
           tile_size, >=3), where `tile_size` is usually 256 or 512
         - encoding: (str): Either 'mapbox' or 'terrarium', the two main RGB
           encodings for elevation values.
+        - backfill: (bool): Whether to create an array of size (tile_size +
+          1)^2, backfilling the bottom and right edges. This is used because
+          Martini needs a grid of size 2^n + 1
 
     Returns:
         (np.array) Array of shape (tile_size^2) with decoded elevation values
@@ -21,9 +24,6 @@ def decode_ele(png, encoding):
     if png.shape[0] <= 4:
         png = png.T
 
-    grid_size = png.shape[0] + 1
-    terrain = np.zeros((grid_size, grid_size), dtype=np.float32)
-
     # Get bands
     if encoding == 'mapbox':
         red = png[:, :, 0] * (256 * 256)
@@ -31,20 +31,30 @@ def decode_ele(png, encoding):
         blue = png[:, :, 2]
 
         # Compute float height
-        heights = (red + green + blue) / 10 - 10000
+        terrain = (red + green + blue) / 10 - 10000
     elif encoding == 'terrarium':
         red = png[:, :, 0] * (256)
         green = png[:, :, 1]
         blue = png[:, :, 2] / 256
 
         # Compute float height
-        heights = (red + green + blue) - 32768
+        terrain = (red + green + blue) - 32768
+
+    if backfill:
+        terrain = compute_backfill(terrain)
+
+    return terrain.flatten('C')
+
+
+def compute_backfill(arr):
+    grid_size = arr.shape[0] + 1
+
+    terrain = np.zeros((grid_size, grid_size), dtype=np.float32)
 
     # Copy to larger array to allow backfilling
-    np.copyto(terrain[:grid_size - 1, :grid_size - 1], heights)
+    np.copyto(terrain[:grid_size - 1, :grid_size - 1], arr)
 
     # backfill right and bottom borders
     terrain[grid_size - 1, :] = terrain[grid_size - 2, :]
     terrain[:, grid_size - 1] = terrain[:, grid_size - 2]
-
-    return terrain.flatten('C')
+    return terrain
