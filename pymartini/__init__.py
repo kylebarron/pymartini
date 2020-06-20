a28afa38af34ed28a13a0205c027f768ccd27929
Kyle Barron <kylebarron2@gmail.com>
2020-06-20 14:27:22 -0600

    Bump version: 0.3.0 → 0.3.1

--- pymartini/__init__.py
+++ pymartini/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Kyle Barron"""
 __email__ = 'kylebarron2@gmail.com'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 from .martini import Martini
 from .util import decode_ele, rescale_positions
