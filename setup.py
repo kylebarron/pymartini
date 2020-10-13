52ba33bfbbd37b7e5fcaa94568f9dba74049eff2
Kyle Barron <kylebarron2@gmail.com>
2020-10-13 10:18:30 -0600

    Build wheels on Python 3.9

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ def find_pyx(path='.'):
 setup(
     name="pymartini",
     version="0.3.2",
+    python_requires=">=3.6",
     description="A Python port of Martini for fast terrain mesh generation",
     long_description=readme,
     long_description_content_type="text/markdown",
